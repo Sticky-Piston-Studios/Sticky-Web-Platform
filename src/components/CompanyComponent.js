92843cf459af8217ab6b7728f096b336ae82a50c
Jakub Duchniewicz <j.duchniewicz@gmail.com>
2023-11-28 20:33:51 +0100

    Fix styles of the list.
    Signed-off-by: Jakub Duchniewicz <j.duchniewicz@gmail.com>

--- src/components/CompanyComponent.js
+++ src/components/CompanyComponent.js
@@ -2,35 +2,40 @@ import React, { useEffect, useState } from "react";
 import styles from "../styles/Company.module.css";
 
 function CompanyComponent() {
-  const [company, setCompany] = useState(null);
+  const [companies, setCompanies] = useState([]);
+  const [newCompany, setNewCompany] = useState({ Name: "", Value: "" });
 
-  useEffect(() => {
-    // Call the "GetCompany" endpoint when the component mounts
-    fetch("/api/companies/GetCompany")
+  const fetchCompanies = () => {
+    // Call the "GetCompanies" endpoint
+    fetch("/api/companies/GetCompanies")
       .then((response) => response.json())
       .then((data) => {
-        setCompany(data);
+        setCompanies(data);
       })
       .catch((error) => {
-        console.error("Error fetching company", error);
+        console.error("Error fetching companies", error);
       });
+  };
+
+  useEffect(() => {
+    fetchCompanies();
   }, []);
 
-  const addCompany = () => {
+  const addCompany = (event) => {
+    event.preventDefault();
+
     // Call the "AddCompany" endpoint
     fetch("/api/companies/AddCompany", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({
-        Name: "New Company",
-        Value: 100,
-      }),
+      body: JSON.stringify(newCompany),
     })
       .then((response) => response.json())
       .then((data) => {
         console.log("Company added", data);
+        fetchCompanies(); // Refetch the companies
       })
       .catch((error) => {
         console.error("Error adding company", error);
@@ -40,18 +45,44 @@ function CompanyComponent() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1 className={styles.title}>Company</h1>
-        {company ? (
-          <div>
-            <p className={styles.description}>Name: {company.Name}</p>
-            <p className={styles.description}>Value: {company.Value}</p>
-          </div>
+        <h1 className={styles.title}>Companies</h1>
+        {companies.length > 0 ? (
+          <ul>
+            {companies.map((company, index) => (
+              <li key={index}>
+                <p className={styles.description}>Name: {company.Name}</p>
+                <p className={styles.description}>Value: {company.Value}</p>
+              </li>
+            ))}
+          </ul>
         ) : (
           <p className={styles.description}>Loading...</p>
         )}
-        <button className={styles.card} onClick={addCompany}>
-          Add Company
-        </button>
+        <form className={styles.form} onSubmit={addCompany}>
+          <input
+            type="text"
+            className={styles.input}
+            value={newCompany.Name}
+            onChange={(e) =>
+              setNewCompany({ ...newCompany, Name: e.target.value })
+            }
+            placeholder="Company Name"
+            required
+          />
+          <input
+            type="number"
+            className={styles.input}
+            value={newCompany.Value}
+            onChange={(e) =>
+              setNewCompany({ ...newCompany, Value: e.target.value })
+            }
+            placeholder="Company Value"
+            required
+          />
+          <button type="submit" className={styles.card}>
+            Add Company
+          </button>
+        </form>
       </main>
     </div>
   );
