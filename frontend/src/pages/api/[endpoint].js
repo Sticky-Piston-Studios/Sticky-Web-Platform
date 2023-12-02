1df2d17f3c4f6963a2bfcbbe117382435242b5bd
Jakub Duchniewicz <j.duchniewicz@gmail.com>
2023-12-02 19:07:21 +0100

    Small refactor of handler in the endpoint.
    Signed-off-by: Jakub Duchniewicz <j.duchniewicz@gmail.com>

--- frontend/src/pages/api/[endpoint].js
+++ frontend/src/pages/api/[endpoint].js
@@ -13,12 +13,73 @@ function bsonToJson(data) {
   });
 }
 
+// Parse the configuration file
+const config = JSON.parse(fs.readFileSync("../configuration.json", "utf8"));
+
+async function callEndpoint(url, req, endpointBody) {
+  // Determine the body to send based on the request method
+  let body = null;
+  if (req.method === "POST" || req.method === "PATCH") {
+    // Check if all required fields are present in the request body
+    const missingFields = endpointBody.Fields.filter(
+      (field) => !(field.Name in req.body)
+    );
+    if (missingFields.length > 0) {
+      // If there are missing fields, return error
+      const missingFieldNames = missingFields.map((field) => field.Name);
+      return {
+        status: 400,
+        body: {
+          error: "Missing required fields: " + missingFieldNames.join(", "),
+        },
+      };
+    }
+
+    // Check if there are any additional fields in the request body
+    const additionalFields = Object.keys(req.body).filter(
+      (field) =>
+        !endpointBody.Fields.some(
+          (requiredField) => requiredField.Name === field
+        )
+    );
+    if (additionalFields.length > 0) {
+      // If there are additional fields, return error
+      return {
+        status: 400,
+        body: {
+          error: "Additional fields: " + additionalFields.join(", "),
+        },
+      };
+    }
+
+    body = JSON.stringify(req.body);
+  }
+
+  // Call the external API
+  const response = await fetch(url, {
+    method: req.method,
+    headers: {
+      ...req.headers, // Forward all headers from the original request
+      "Content-Type": "application/json",
+    },
+    body: body, // Send the body for POST and PATCH requests
+  });
+
+  // Get the data from the external API response
+  const data = await response.json();
+
+  // Convert the data to BSON HACK!
+  data.value.data = bsonToJson(data.value.data);
+
+  return {
+    status: response.status,
+    body: data,
+  };
+}
+
+// TODO: this currently supports one level of nesting, we can create more levels adjusting the /api/<multiple_levels/>/concreteendpoint.js
 export default async function handler(req, res) {
   try {
-    console.log("req.query: ", req.query);
-    // Parse the configuration file
-    const config = JSON.parse(fs.readFileSync("../configuration.json", "utf8"));
-
     // Construct the path from the subroute
     const path = `/api/${req.query.endpoint}`;
 
@@ -39,77 +100,18 @@ export default async function handler(req, res) {
       return;
     }
 
-    console.log(API_URL);
-    // Forward the request to the external API
-    //const queryString = new URLSearchParams(req.query).toString();
-    //const url = queryString
-    //  ? `${API_URL}${endpointGroup.Path}/?${queryString}`
-    //  : `${API_URL}${endpointGroup.Path}`;
     const url = `${API_URL}${path}`;
-    console.log("url: ", url);
 
-    // Determine the body to send based on the request method
-    let body = null;
-    if (req.method === "POST" || req.method === "PATCH") {
-      // check if all required fields are present
-      // read them from the dynamicCofiguration from the EndpointBodies with given endpoint name
-      const endpointBody = config.EndpointBodies.find(
-        (e) => e.Name === endpointConfig.Name
-      );
-      console.log("endpoint body: ", endpointBody);
-      // check if all fields in the config are present in the request body, also check that no additional fields are present
-      const requiredFields = endpointBody.Fields;
-      // check if all required fields are present in the request body
-      const missingFields = requiredFields.filter(
-        (field) => !(field.Name in req.body)
-      );
-      if (missingFields.length > 0) {
-        // if there are missing fields, return error
-        const missingFieldNames = missingFields.map((field) => field.Name);
-        res.status(400).json({
-          error: "Missing required fields: " + missingFieldNames.join(", "),
-        });
-        return;
-      }
-      // check if there are any additional fields in the request body
-      const additionalFields = Object.keys(req.body).filter(
-        (field) =>
-          !requiredFields.some((requiredField) => requiredField.Name === field)
-      );
-      if (additionalFields.length > 0) {
-        // if there are additional fields, return error
-        res.status(400).json({
-          error: "Additional fields: " + additionalFields.join(", "),
-        });
-        return;
-      }
+    // Find the endpoint body
+    const endpointBody = config.EndpointBodies.find(
+      (e) => e.Name === endpointConfig.Name
+    );
+    console.log("endpoint body: ", endpointBody);
 
-      body = JSON.stringify(req.body);
-    }
+    const result = await callEndpoint(url, req, endpointBody);
 
-    console.log("All fine, can post the body: ", body);
-    console.log("url: ", url);
-
-    // workaround for json to bson conversion
-    const response = await fetch(url, {
-      method: req.method,
-      headers: {
-        ...req.headers, // Forward all headers from the original request
-        "Content-Type": "application/json",
-      },
-      body: body, // Send the body for POST and PATCH requests
-    });
-
-    // Get the data from the external API response
-    const data = await response.json();
-
-    // Convert the data to BSON HACK!
-    data.value.data = bsonToJson(data.value.data);
-
-    console.log("converted the data to bson: ", data);
-
-    // Send the data back as a response
-    res.status(response.status).json(data);
+    // Send the result back as a response
+    res.status(result.status).json(result.body);
   } catch (error) {
     // Handle any errors
     res.status(500).json({ error: "Error forwarding the request" });
